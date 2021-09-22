40cd5ab1d3846b33cff3e1051f903dff2de79306
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-22 18:36:53 -0500

    what is good code?

--- big_O.js
+++ big_O.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    for ( let i = 0; i < array.length; i++) {
+        if(array[i] === 'nemo'){
+            console.log('Found NEMO!');
+        }
+    }
+}
+
+findNemo(nemo)
