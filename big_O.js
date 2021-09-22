87607f068bdc8e924a65f86ed2465c04655d5271
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-22 18:56:09 -0500

    Big o and a small scalability understand

--- big_O.js
+++ big_O.js
@@ -1,11 +1,19 @@
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(1000).fill('nemo');
+
 
 function findNemo(array) {
+    let t0 = performance.now();
     for ( let i = 0; i < array.length; i++) {
         if(array[i] === 'nemo'){
             console.log('Found NEMO!');
         }
     }
+    let t1 = performance.now();
+    console.log('call to find Nemo took ' + (t1 - t0)
+    + ' milliseconds');
 }
 
-findNemo(nemo)
+findNemo(large)
+
