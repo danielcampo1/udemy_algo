52fca3d8f1b15fb2fb0df02e3a4d12e91e889f3c
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-22 19:08:41 -0500

    O(n) and O(1)

--- big_O.js
+++ big_O.js
@@ -1,19 +1,30 @@
-const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(1000).fill('nemo');
+// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// const large = new Array(1000).fill('nemo');
 
 
-function findNemo(array) {
-    let t0 = performance.now();
-    for ( let i = 0; i < array.length; i++) {
-        if(array[i] === 'nemo'){
-            console.log('Found NEMO!');
-        }
-    }
-    let t1 = performance.now();
-    console.log('call to find Nemo took ' + (t1 - t0)
-    + ' milliseconds');
+// function findNemo(array) {
+//     let t0 = performance.now();
+//     for ( let i = 0; i < array.length; i++) {
+//         if(array[i] === 'nemo'){
+//             console.log('Found NEMO!');
+//         }
+//     }
+//     let t1 = performance.now();
+//     console.log('call to find Nemo took ' + (t1 - t0)
+//     + ' milliseconds');
+// }
+
+// findNemo(large) 
+
+// O(n) --> linear Time
+// O(1) --> constant Time - is a flat time
+
+const boxes = [0,1,2,3,4,5,6];
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
 }
 
-findNemo(large)
-
+logFirstTwoBoxes(boxes) // O(2)
