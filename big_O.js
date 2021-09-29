b5562f800b5e79c515b7e7b93b2949100bd3690a
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-28 20:12:09 -0500

    fresh

--- big_O.js
+++ big_O.js
@@ -20,11 +20,11 @@
 // O(n) --> linear Time
 // O(1) --> constant Time - is a flat time
 
-const boxes = [0,1,2,3,4,5,6];
+// const boxes = [0,1,2,3,4,5,6];
 
-function logFirstTwoBoxes(boxes) {
-    console.log(boxes[0]); // O(1)
-    console.log(boxes[1]); // O(1)
-}
+// function logFirstTwoBoxes(boxes) {
+//     console.log(boxes[0]); // O(1)
+//     console.log(boxes[1]); // O(1)
+// }
 
-logFirstTwoBoxes(boxes) // O(2)
+// logFirstTwoBoxes(boxes) // O(2)
